b0c1ae4aae0861996c036c3f95b29827a60943f1
jon <jonprosser84@gmail.com>
2021-08-05 21:10:43 +0100

    added rgba function

--- src/App.js
+++ src/App.js
@@ -4,30 +4,49 @@ import SingleColor from './SingleColor'
 import Values from 'values.js'
 
 function App() {
-
   const [color, setColor] = useState('')
   const [error, setError] = useState(false)
-  const [list, setList] = useState([])
+  const [list, setList] = useState(new Values('#f15025').all(10))
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    console.log('hello')
+    try {
+      let colors = new Values(color).all(10)
+      setList(colors)
+    } catch (error) {
+      setError(true)
+      console.log(error)
+    }
   }
 
   return (
     <>
-      <section className="container">
-        <h3>Color Generator</h3>
-
+      <section className='container'>
+        <h3>color generator</h3>
         <form onSubmit={handleSubmit}>
-          <input type="text" value={color} onChange={(e) => setColor(e.target.value)} placeholder='#333333' />
-
-          <button className="btn" type="submit">Submit</button>
+          <input
+            type='text'
+            value={color}
+            onChange={(e) => setColor(e.target.value)}
+            placeholder='#f15025'
+            className={`${error ? 'error' : null}`}
+          />
+          <button className='btn' type='submit'>
+            submit
+          </button>
         </form>
-
       </section>
-      <section className="colors">
-        <h4>List goes here</h4>
+      <section className='colors'>
+        {list.map((color, index) => {
+          return (
+            <SingleColor
+              key={index}
+              {...color}
+              index={index}
+              hexColor={color.hex}
+            />
+          )
+        })}
       </section>
     </>
   )
